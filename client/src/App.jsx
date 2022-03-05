e4be2673258cc1c1ced2243bc5684c4d2170e76c
Ishank Chopra <ishankchopra@gmail.com>
2022-03-06 01:16:53 +0530

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
-require("dotenv").config();
+
 const App = () => (
   <div className="min-h-screen">
     <div className="gradient-bg-welcome">
